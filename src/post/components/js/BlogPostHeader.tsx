3241ae9b93a7b5a6e727d76ed6a0ec14ba4df350
esum <salvae00027@gmail.com>
2022-11-26 22:43:55 +0900

    Add type for props of BlogPostHeader
    Issue #6 Create Blog Post Header(Title) component

--- src/post/components/js/BlogPostHeader.tsx
+++ src/post/components/js/BlogPostHeader.tsx
@@ -1,5 +1,20 @@
-function BlogPostHeader() {
+type BlogPostHeaderProps = {
+  title: string;
+  creationDate: number;
+  estimatedTimeToRead: number;
+  tagList: string[];
+  thumbnailUrl: string;
+};
+
+function BlogPostHeader({
+  title,
+  creationDate,
+  estimatedTimeToRead,
+  tagList,
+  thumbnailUrl,
+}: BlogPostHeaderProps) {
   return <>{/*TODO: Show header information*/}</>;
 }
 
 export { BlogPostHeader };
+export type { BlogPostHeaderProps };
