ca6ebeacb230337b3f240c6221c22964929c5e67
esum <salvae00027@gmail.com>
2022-11-27 00:29:50 +0900

    Add prop type for Header tag item
    Issue #6 Create Blog Post Header(Title) component

--- src/post/components/js/BlogPostHeaderTagItem.tsx
+++ src/post/components/js/BlogPostHeaderTagItem.tsx
@@ -1,5 +1,10 @@
-function BlogPostHeaderTagItem() {
+type BlogPostHeaderTagItemProps = {
+  tagName: string;
+};
+
+function BlogPostHeaderTagItem({ tagName }: BlogPostHeaderTagItemProps) {
   return <div>{/*TODO : Show tag name*/}</div>;
 }
 
 export { BlogPostHeaderTagItem };
+export type { BlogPostHeaderTagItemProps };
