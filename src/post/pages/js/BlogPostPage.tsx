e283e83d34a851382af575a99dc47b31d59161a2
esum <salvae00027@gmail.com>
2022-11-27 11:25:53 +0900

    Add sample image and props to test BlogPostHeader
    Issue #6 Create Blog Post Header(Title) component

--- src/post/pages/js/BlogPostPage.tsx
+++ src/post/pages/js/BlogPostPage.tsx
@@ -1,5 +1,38 @@
+import {
+  BlogPostHeader,
+  BlogPostHeaderProps,
+} from '../../components/js/BlogPostHeader';
+import sampleImage from '../../../assets/headerSampleImage.png';
+
 function BlogPostPage() {
-  return <div></div>;
+  // TODO : Temporary prop data to show header.
+  // We need to replace it to redux data.
+  const testProps: BlogPostHeaderProps = {
+    title: 'title',
+    creationDate: Date.now(),
+    estimatedTimeToRead: 10,
+    tagList: [
+      {
+        tagId: 1,
+        tagProps: { tagName: 'test1' },
+      },
+      {
+        tagId: 2,
+        tagProps: { tagName: 'test2' },
+      },
+      {
+        tagId: 3,
+        tagProps: { tagName: 'test3' },
+      },
+    ],
+    thumbnailUrl: sampleImage,
+  };
+
+  return (
+    <div>
+      <BlogPostHeader {...testProps} />
+    </div>
+  );
 }
 
 export { BlogPostPage };
