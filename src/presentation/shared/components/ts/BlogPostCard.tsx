f0b6123e2d124e15a9d1cf19feb8a4dcbb5cd093
MoYeonWook <moyu0306@gmail.com>
2022-12-11 01:30:46 +0900

    Add BlogPostCard and BlogPostCardProps
    related : Create Blog post card component #5

---
+++ src/presentation/shared/components/ts/BlogPostCard.tsx
@@ -0,0 +1,32 @@
+//TODO import css
+
+import { getFormattedDate } from '../../utils/PostUtils';
+
+type BlogPostCardProps = {
+  id: number;
+  title: string;
+  creationDate: number;
+  estimateTimedToRead: number;
+  thumbnailUrl: string;
+};
+
+function BlogPostCard({
+  title,
+  creationDate,
+  estimateTimedToRead,
+  thumbnailUrl,
+}: BlogPostCardProps) {
+  return (
+    <div className="Post-card">
+      <img src={thumbnailUrl} alt="${title}_thumbnail" />
+      <div className="Card-content">
+        <div>{getFormattedDate(creationDate)}</div>
+        <h3>{title}</h3>
+        <div>{estimateTimedToRead}min</div>
+      </div>
+    </div>
+  );
+}
+
+export { BlogPostCard };
+export type { BlogPostCardProps };
