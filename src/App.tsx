18d860db6759de828632e8eace7e468eadbe2216
woojinger <tlsdnwls1253@gmail.com>
2022-12-11 13:36:40 +0900

    Apply react-router-dom
    Issue #16
    - "/": BlogHomePostList
    - "/post": BlogPostPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { BlogHeader } from "./presentation/header/components/ts/BlogHeader";
 import { BlogPostPage } from "./presentation/post/pages/ts/BlogPostPage";
-import { BlogHomePostList } from './presentation/home/components/ts/BlogHomePostList';
+import { BlogHomePostList } from "./presentation/home/components/ts/BlogHomePostList";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <BlogHomePostList />,
+  },
+  {
+    path: "/post",
+    element: <BlogPostPage />,
+  },
+]);
 
 function App() {
   return (
     <div>
       <BlogHeader />
-      <BlogPostPage />
-      <BlogHomePostList/>
+      <RouterProvider router={router} />
     </div>
   );
 }
