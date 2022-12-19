0eb03e0be863859cf6d1f28527fd210f48786941
woojinger <tlsdnwls1253@gmail.com>
2022-12-20 00:33:18 +0900

    Implement Error Page
    Issue #14

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,22 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { BlogHeader } from "./presentation/header/components/ts/BlogHeader";
 import { BlogPostPage } from "./presentation/post/pages/ts/BlogPostPage";
 import { BlogHomePostList } from "./presentation/home/components/ts/BlogHomePostList";
-import { ErrorPage } from "./presentation/pages/ts/ErrorPage";
+import { ErrorPage, ErrorPageProps } from "./presentation/pages/ts/ErrorPage";
 import { EmptyPage, EmptyPageProps } from "./presentation/pages/ts/EmptyPage";
 
 const emptyPageProps: EmptyPageProps = {
   msg: "작성된 포스트(검색 결과)가 없습니다.",
 };
 
+const errorPageProps: ErrorPageProps = {
+  msg: "일시적인 오류가 발생했습니다.",
+};
+
 const router = createBrowserRouter([
   {
     path: "/",
     element: <BlogHomePostList />,
-    errorElement: <ErrorPage />,
+    errorElement: <ErrorPage {...errorPageProps} />,
   },
   {
     path: "/post/:id",
@@ -21,7 +25,7 @@ const router = createBrowserRouter([
     loader: ({ params }: any) => {
       return params.id;
     },
-    errorElement: <ErrorPage />,
+    errorElement: <ErrorPage {...errorPageProps} />,
   },
   // temporary link for testing EmptyPage
   {
--- src/presentation/pages/ts/ErrorPage.tsx
+++ src/presentation/pages/ts/ErrorPage.tsx
@@ -1,6 +1,20 @@
-function ErrorPage() {
-  // temporary implementation
-  return <div>"일시적인 오류가 발생했습니다"</div>;
+import loupeImage from '../../../assets/loupeBig.png';
+import '../css/ErrorPage.css';
+
+type ErrorPageProps = {
+  msg: string;
+};
+
+function ErrorPage({ msg }: ErrorPageProps) {
+  return (
+    <div>
+      <div className="loupe-container">
+        <img className="loupe" src={loupeImage} alt={'No item exist'}></img>
+      </div>
+      <div className="msg">{msg}</div>
+    </div>
+  );
 }
 
 export { ErrorPage };
+export type { ErrorPageProps };
