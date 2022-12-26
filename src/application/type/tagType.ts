dc0545f3be393920e4f425509cf194db4db9330a
MoYeonWook <moyu0306@gmail.com>
2022-12-26 21:09:38 +0900

    Apply redux to BlogHomePostList Component

--- src/application/type/tagType.ts
+++
@@ -1,6 +0,0 @@
-type tagType = {
-  tagId: number;
-  tagName: String;
-};
-
-export type { tagType };
--- src/presentation/home/components/ts/BlogHomePostList.tsx
+++ src/presentation/home/components/ts/BlogHomePostList.tsx
@@ -1,97 +1,13 @@
 /* temporary test page for BlogPostCard */
 import '../css/BlogHomePostList.css';
-import {
-  BlogPostCard,
-  BlogPostCardProps,
-} from '../../../shared/components/ts/BlogPostCard';
-import sampleImage from '../../../../assets/postCardSampleImg.png';
-
-const posts: any = [
-  // temporary testdata , TODO : Create BlogPostType in the application layer
-  {
-    id: 1,
-    postCardProps: {
-      title: 'some title',
-      creationDate: Date.now(),
-      estimateTimedToRead: 10,
-      thumbnailUrl: sampleImage,
-    },
-  },
-  {
-    id: 2,
-    postCardProps: {
-      title: 'some title',
-      creationDate: Date.now(),
-      estimateTimedToRead: 10,
-      thumbnailUrl: sampleImage,
-    },
-  },
-  {
-    id: 3,
-    postCardProps: {
-      title: 'some title',
-      creationDate: Date.now(),
-      estimateTimedToRead: 10,
-      thumbnailUrl: sampleImage,
-    },
-  },
-  {
-    id: 4,
-    postCardProps: {
-      title: 'some title',
-      creationDate: Date.now(),
-      estimateTimedToRead: 10,
-      thumbnailUrl: sampleImage,
-    },
-  },
-  {
-    id: 5,
-    postCardProps: {
-      title: 'some title',
-      creationDate: Date.now(),
-      estimateTimedToRead: 10,
-      thumbnailUrl: sampleImage,
-    },
-  },
-  {
-    id: 6,
-    postCardProps: {
-      title: 'some title',
-      creationDate: Date.now(),
-      estimateTimedToRead: 10,
-      thumbnailUrl: sampleImage,
-    },
-  },
-  {
-    id: 7,
-    postCardProps: {
-      title: 'some title',
-      creationDate: Date.now(),
-      estimateTimedToRead: 10,
-      thumbnailUrl: sampleImage,
-    },
-  },
-  {
-    id: 8,
-    postCardProps: {
-      title: 'some title',
-      creationDate: Date.now(),
-      estimateTimedToRead: 10,
-      thumbnailUrl: sampleImage,
-    },
-  },
-  {
-    id: 9,
-    postCardProps: {
-      title: 'some title',
-      creationDate: Date.now(),
-      estimateTimedToRead: 10,
-      thumbnailUrl: sampleImage,
-    },
-  },
-];
+import { BlogPostCard } from '../../../shared/components/ts/BlogPostCard';
+import { useSelector } from 'react-redux';
+import { selectPosts } from '../../../../application/redux/HomePosts/homePostsSlice';
+import { simplePostTypeToHomePost } from '../../../../application/api/mappers/simplePostTypeToHomePost';
 
 function BlogHomePostList() {
+  const posts: any[] = useSelector(selectPosts).map(simplePostTypeToHomePost);
+
   return (
     <div className="Card-container">
       {posts.map((post: any) => (
