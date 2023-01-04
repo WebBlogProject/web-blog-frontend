bdce035f042fcd67a95b9f066bbe0679e6ebd46f
MoYeonWook <moyu0306@gmail.com>
2023-01-04 15:25:34 +0900

    Update store's reducer and middleware
    related #19

--- src/application/redux/store.ts
+++ src/application/redux/store.ts
@@ -1,16 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import blogHomePostsReducer from './HomePosts/homePostsSlice';
-import blogSearchResultsPostsReducer from './SearchResultPosts/searchResultPostsSlice';
+import { apiSlice } from './api/apiSlice';
 
 const store = configureStore({
   reducer: {
-    blogHomePosts: blogHomePostsReducer,
-    blogSearchResultPosts: blogSearchResultsPostsReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
-export default store;
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export { store };
