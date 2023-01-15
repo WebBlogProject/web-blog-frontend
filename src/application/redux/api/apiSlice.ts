132c97888d4a143c75c2cf6ddd2186246f3a47af
esum <salvae00027@gmail.com>
2023-01-15 17:50:09 +0900

    Add query and mock data for getting post by id
    Issue: #24 Render BlogPostPage with post date from redux state

--- src/application/redux/api/apiSlice.ts
+++ src/application/redux/api/apiSlice.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { Post } from '../../types/Post';
 import { PostHeaderData } from '../../types/PostHeaderData';
 
 const apiSlice = createApi({
@@ -10,8 +11,11 @@ const apiSlice = createApi({
     getPostHeaders: builder.query<PostHeaderData[], void>({
       query: () => '/postHeaders.json',
     }),
+    getPostById: builder.query<Post, number>({
+      query: (id: number) => `/post/${id}.json`,
+    }),
   }),
 });
 
 export { apiSlice };
-export const { useGetPostHeadersQuery } = apiSlice;
+export const { useGetPostHeadersQuery, useGetPostByIdQuery } = apiSlice;
