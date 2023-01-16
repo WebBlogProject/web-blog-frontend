93d2db5b20f9cd731e0afa5f4e35b5e165f4ae75
esum <salvae00027@gmail.com>
2023-01-16 23:28:28 +0900

    Navigate to BlogPost page when click post card
    Issue: #24 Render BlogPostPage with post date from redux state

--- src/presentation/home/components/ts/BlogHomePostList.tsx
+++ src/presentation/home/components/ts/BlogHomePostList.tsx
@@ -18,6 +18,7 @@ function BlogHomePostList() {
       <div className="Card-container">
         {posts.map((post: PostPreview) => (
           <BlogPostCard
+            id={post.id}
             title={post.title}
             creationDate={post.creationDate}
             estimatedTimeToRead={post.estimatedTimeToRead}
--- src/presentation/shared/components/ts/BlogPostCard.tsx
+++ src/presentation/shared/components/ts/BlogPostCard.tsx
@@ -1,8 +1,11 @@
 import '../css/BlogPostCard.css';
 
 import { getFormattedDate } from '../../utils/PostUtils';
+import { useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
 
 type BlogPostCardProps = {
+  id: number;
   title: string;
   creationDate: number;
   estimatedTimeToRead: number;
@@ -10,13 +13,20 @@ type BlogPostCardProps = {
 };
 
 function BlogPostCard({
+  id,
   title,
   creationDate,
   estimatedTimeToRead,
   thumbnailUrl,
 }: BlogPostCardProps) {
+  const routerNavigate = useNavigate();
+
+  const onClickPostCard = useCallback(() => {
+    routerNavigate(`/post/${id}`);
+  }, [id, routerNavigate]);
+
   return (
-    <div className="Post-card">
+    <div className="Post-card" onClick={onClickPostCard}>
       <img src={thumbnailUrl} alt={`${title}_thumbnail`} />
       <div className="Card-content">
         <h3>{title}</h3>
