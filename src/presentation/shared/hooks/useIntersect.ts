9bd5fdeb222c69b652fec220aac2c190e91d0912
MoYeonWook <moyu0306@gmail.com>
2023-02-04 16:26:08 +0900

    Create useIntersect custom hook
    This is part of an infinite scroll implementation.
     Intersection observer API will detect
     that the last element of component has shown on the viewport.
    
     related: #25

---
+++ src/presentation/shared/hooks/useIntersect.ts
@@ -0,0 +1,33 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+type IntersectHandler = (
+  entry: IntersectionObserverEntry,
+  observer: IntersectionObserver
+) => void;
+
+const useIntersect = (
+  onIntersect: IntersectHandler,
+  options?: IntersectionObserverInit
+) => {
+  const callback = useCallback(
+    (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) onIntersect(entry, observer);
+      });
+    },
+    [onIntersect]
+  );
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, [ref, options, callback]);
+
+  return ref;
+};
+
+export { useIntersect };
