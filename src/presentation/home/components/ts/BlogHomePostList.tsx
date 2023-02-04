8d4d3e2ddcd91f5e37da9fadb57fadc7c9318d97
MoYeonWook <moyu0306@gmail.com>
2023-02-04 16:20:32 +0900

    Add Infinite Scroll to BlogHomePostList
    related: #25

--- src/presentation/home/components/ts/BlogHomePostList.tsx
+++ src/presentation/home/components/ts/BlogHomePostList.tsx
@@ -2,11 +2,27 @@ import '../css/BlogHomePostList.css';
 import { BlogPostCard } from '../../../shared/components/ts/BlogPostCard';
 import { convertToPostPreview } from '../../../../application/mappers/postHeaderMappers';
 import { useGetPostHeadersQuery } from '../../../../application/redux/api/apiSlice';
-import { useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { PostPreview } from '../../../../application/types/PostPreview';
+import { useIntersect } from '../../../shared/hooks/useIntersect';
+import { useFetchPages } from '../../../shared/hooks/useFetchPages';
+import { ErrorPage, ErrorPageProps } from '../../../pages/ts/ErrorPage';
 
 function BlogHomePostList() {
-  const { data, isSuccess } = useGetPostHeadersQuery();
+  const [pageNumber, setPageNumber] = useState(1);
+  const { data, isSuccess, isLoading, isError, hasNextResult } = useFetchPages(
+    useGetPostHeadersQuery,
+    pageNumber
+  );
+
+  const ref = useIntersect(
+    useCallback(
+      async (entry, observer) => {
+        if (hasNextResult && !isLoading) setPageNumber(pageNumber + 1);
+      },
+      [hasNextResult, isLoading, pageNumber]
+    )
+  );
 
   const posts: PostPreview[] = useMemo(() => {
     const emptyArray: PostPreview[] = [];
@@ -15,17 +31,20 @@ function BlogHomePostList() {
 
   if (isSuccess) {
     return (
-      <div className="Card-container">
-        {posts.map((post: PostPreview) => (
-          <BlogPostCard
-            id={post.id}
-            title={post.title}
-            creationDate={post.creationDate}
-            estimatedTimeToRead={post.estimatedTimeToRead}
-            thumbnailUrl={post.thumbnailUrl}
-            key={post.id}
-          />
-        ))}
+      <div>
+        <div className="Card-container">
+          {posts.map((post: PostPreview) => (
+            <BlogPostCard
+              id={post.id}
+              title={post.title}
+              creationDate={post.creationDate}
+              estimatedTimeToRead={post.estimatedTimeToRead}
+              thumbnailUrl={post.thumbnailUrl}
+              key={post.id}
+            />
+          ))}
+        </div>
+        <div style={{ height: '1px' }} ref={ref} />
       </div>
     );
   } else {
