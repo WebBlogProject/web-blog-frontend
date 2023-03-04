4f5f050c6db76d98dc57947e1001e092d039ec00
MoYeonWook <moyu0306@gmail.com>
2023-03-05 00:35:51 +0900

    Add homeSlice to store.ts

--- src/application/redux/store.ts
+++ src/application/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import { apiSlice } from './api/apiSlice';
+import { homeSlice } from './home/homeSlice';
 
 const store = configureStore({
   reducer: {
+    [homeSlice.name]: homeSlice.reducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
