954702e8412e13325b2e013a34f6f6490bc0c508
신우진 <48384542+Woojinger@users.noreply.github.com>
2023-03-26 14:20:32 +0900

    Implement going to the main page on header clicks
    Issue #28

--- src/presentation/header/components/ts/BlogHeader.tsx
+++ src/presentation/header/components/ts/BlogHeader.tsx
@@ -1,12 +1,22 @@
 import { BlogHeaderSearchBar } from './BlogHeaderSearchBar';
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../css/BlogHeader.css';
 
 const blogName = 'Blog Name';
 
 function BlogHeader() {
+  const routerNavigate = useNavigate();
+
+  const onClickSearchButton = useCallback(() => {
+    routerNavigate(`/`);
+  }, [routerNavigate]);
+
   return (
     <div>
-      <div className="BlogName">{blogName}</div>
+      <div className="BlogName" onClick={onClickSearchButton}>
+        {blogName}
+      </div>
       <BlogHeaderSearchBar />
       <hr />
     </div>
