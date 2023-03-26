3794e4a53b3dce390d8ab55ff28c44fe2bafb15d
신우진 <48384542+Woojinger@users.noreply.github.com>
2023-03-26 14:16:26 +0900

    Implement search with RTKQuery
    Issue #28

--- src/application/redux/api/apiSlice.ts
+++ src/application/redux/api/apiSlice.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Post } from '../../types/Post';
 import { PostHeaderPage } from '../../types/PostHeaderPage';
+import { PostHeaderData } from '../../types/PostHeaderData';
 
 const apiSlice = createApi({
   reducerPath: 'api',
@@ -14,8 +15,15 @@ const apiSlice = createApi({
     getPostById: builder.query<Post, number>({
       query: (id: number) => `/post/${id}.json`,
     }),
+    searchPostHeaders: builder.query<PostHeaderData[], string>({
+      query: (query: string) => `/query/${query}.json`,
+    }),
   }),
 });
 
 export { apiSlice };
-export const { useLazyGetPostHeadersQuery, useGetPostByIdQuery } = apiSlice;
+export const {
+  useLazyGetPostHeadersQuery,
+  useGetPostByIdQuery,
+  useSearchPostHeadersQuery,
+} = apiSlice;
--- src/presentation/searchPage/pages/ts/SearchResultPage.tsx
+++ src/presentation/searchPage/pages/ts/SearchResultPage.tsx
@@ -3,88 +3,31 @@ import { PostHeaderData } from '../../../../application/types/PostHeaderData';
 import { useCallback } from 'react';
 import { BlogPostCardList } from '../../../shared/components/ts/BlogPostCardList';
 import { KeywordBar } from '../../components/ts/KeywordBar';
-
-const mockPostHeader: PostHeaderData[] = [
-  {
-    id: 1,
-    title: 'Blog 1',
-    creationDate: 1000,
-    estimatedTimeToRead: 3,
-    thumbnailUrl: 'someUrl.png',
-    tagList: [
-      {
-        tagId: 1,
-        tagName: 'tag1',
-      },
-    ],
-  },
-  {
-    id: 2,
-    title: 'Blog 2',
-    creationDate: 2000,
-    estimatedTimeToRead: 6,
-    thumbnailUrl: 'someUrl.png',
-    tagList: [
-      {
-        tagId: 2,
-        tagName: 'tag2',
-      },
-    ],
-  },
-  {
-    id: 3,
-    title: 'Blog 3',
-    creationDate: 3000,
-    estimatedTimeToRead: 9,
-    thumbnailUrl: 'someUrl.png',
-    tagList: [
-      {
-        tagId: 3,
-        tagName: 'tag3',
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: 'Blog 4',
-    creationDate: 4000,
-    estimatedTimeToRead: 12,
-    thumbnailUrl: 'someUrl.png',
-    tagList: [
-      {
-        tagId: 4,
-        tagName: 'tag4',
-      },
-    ],
-  },
-  {
-    id: 5,
-    title: 'Blog 5',
-    creationDate: 5000,
-    estimatedTimeToRead: 15,
-    thumbnailUrl: 'someUrl.png',
-    tagList: [
-      {
-        tagId: 5,
-        tagName: 'tag5',
-      },
-    ],
-  },
-];
+import { useSearchPostHeadersQuery } from '../../../../application/redux/api/apiSlice';
+import { ErrorPage } from '../../../pages/ts/ErrorPage';
 
 function SearchResultPage() {
   const [search, setSearch] = useSearchParams();
-  // get query from url
   const query = search.get('q') ?? '';
+
+  const { data, isSuccess, isError } = useSearchPostHeadersQuery(query);
+
   const renderPage = useCallback(() => {
-    return (
-      <div>
-        <KeywordBar keyword={query} />
-        <BlogPostCardList posts={mockPostHeader} />
-      </div>
-    );
-  }, [query]);
-  return <div>{renderPage()}</div>;
+    if (isSuccess) {
+      const postHeaders = data ?? ({} as PostHeaderData[]);
+      return (
+        <div>
+          <KeywordBar keyword={query} />
+          <BlogPostCardList posts={postHeaders} />
+        </div>
+      );
+    } else if (isError) {
+      return <ErrorPage msg={'검색에 실패했습니다.'} />;
+    } else {
+      return <div> loading ... </div>;
+    }
+  }, [query, isSuccess, isError]);
+  return renderPage();
 }
 
 export { SearchResultPage };
